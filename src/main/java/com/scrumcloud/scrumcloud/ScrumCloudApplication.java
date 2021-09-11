abd1b614b2a4b7a502b86e67d7f7f934f5166d96
machadondavi <machadondavi@gmail.com>
2021-09-10 21:46:01 -0300

    criando projeto e adicionando algumas dependencias

---
+++ src/main/java/com/scrumcloud/scrumcloud/ScrumCloudApplication.java
@@ -0,0 +1,13 @@
+package com.scrumcloud.scrumcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scrumcloud/scrumcloud/ScrumCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrumcloud.scrumcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
