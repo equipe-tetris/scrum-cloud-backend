db87fa21e2e102f4586f3577436f32341bb14eda
machadondavi <machadondavi@gmail.com>
2021-10-19 08:43:50 -0300

    SC-15

---
+++ src/main/java/com/scrumcloud/scrumcloud/websockets/Message.java
@@ -0,0 +1,13 @@
+package com.scrumcloud.scrumcloud.websockets;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+
+    private String name;
+}
