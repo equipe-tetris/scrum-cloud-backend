144b5991ef7088d6d5f301da895ed15920d26281
machadondavi <machadondavi@gmail.com>
2021-11-30 15:56:13 -0300

    atualizando sql

--- scripts.sql
+++ scripts.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE `scrumcloud`
+
 CREATE TABLE `usuario` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
@@ -32,28 +34,33 @@ CREATE TABLE `integrantes_equipe` (
 CREATE TABLE `sala_planning` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
-  `scrum_master` bigint NOT NULL,
   `data_criacao` date NOT NULL,
   `metrica_sala` varchar(20) NOT NULL,
   `equipe` bigint NOT NULL,
+  `usuario` bigint DEFAULT NULL,
+  `sm_provisorio` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id_fk_equipe` (`equipe`),
-  KEY `id_fk_usuario` (`scrum_master`),
+  KEY `sala_planning_FK` (`usuario`),
+  KEY `sala_planning_FK_1` (`sm_provisorio`),
   CONSTRAINT `id_fk_equipe` FOREIGN KEY (`equipe`) REFERENCES `equipe` (`id`),
-  CONSTRAINT `id_fk_usuario` FOREIGN KEY (`scrum_master`) REFERENCES `usuario` (`id`)
+  CONSTRAINT `sala_planning_FK` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`id`),
+  CONSTRAINT `sala_planning_FK_1` FOREIGN KEY (`sm_provisorio`) REFERENCES `usuario` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- scrumcloud.tasks definition
 
-CREATE TABLE `tasks` (
+CREATE TABLE `task` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `sala_planning` bigint NOT NULL,
   `conteudo` varchar(100) NOT NULL,
   `data_criacao` date DEFAULT NULL,
+  `status` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `valor_final` varchar(15) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `tasks_FK` (`sala_planning`),
-  CONSTRAINT `tasks_FK` FOREIGN KEY (`sala_planning`) REFERENCES `sala_planning` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `task_FK` (`sala_planning`),
+  CONSTRAINT `task_FK` FOREIGN KEY (`sala_planning`) REFERENCES `sala_planning` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- scrumcloud.task_votacao definition
 
@@ -67,4 +74,16 @@ CREATE TABLE `task_votacao` (
   KEY `task_votacao_FK_1` (`usuario`),
   CONSTRAINT `task_votacao_FK` FOREIGN KEY (`task`) REFERENCES `task` (`id`),
   CONSTRAINT `task_votacao_FK_1` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `alerta` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `mensagem_alerta` varchar(100) NOT NULL,
+  `sala_planning` bigint DEFAULT NULL,
+  `usuario` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `alerta_FK` (`sala_planning`),
+  KEY `alerta_FK_1` (`usuario`),
+  CONSTRAINT `alerta_FK` FOREIGN KEY (`sala_planning`) REFERENCES `sala_planning` (`id`),
+  CONSTRAINT `alerta_FK_1` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
